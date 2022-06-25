d9b08cb6488e20d67d204e211b8d02dffc86c6c8
paco mendoza tamayo <franciscomendozatamayo@gmail.com>
2022-06-25 11:40:56 -0500

    Genera Reporte Allure

--- src/test/java/config/OwaspConfig.java
+++ src/test/java/config/OwaspConfig.java
@@ -4,14 +4,13 @@ public class OwaspConfig {
 
     // host/port
     public static String protocol=ReadConfig.getInstance().getOwaspProtocol();
-    public static String host=ReadConfig.getInstance().getHost();
+    public static String host=ReadConfig.getInstance().getOwaspHost();
     public static String port=ReadConfig.getInstance().getOwaspPort();
-
     // api
     public static final String START_SCANNING=protocol+host+":"+port+"/JSON/ascan/action/scan/?url=";
     public static final String CHECK_STATUS=protocol+host+":"+port+"/JSON/ascan/view/status/?scanId=";
-    public static final String READ_ALERTS=protocol+host+":"+port+"/JSON/alert/view/alerts/?baseurl=";
-    public static final String READ_LIST_ATTACK=protocol+host+":"+port+"/JSON/ascan/view/scanProgress/?scanId=";
+    public static final String READ_ALERTS=protocol+host+":"+port+"/HTML/alert/view/alerts/?baseurl=";
+    public static final String READ_LIST_ATTACK=protocol+host+":"+port+"/HTML/ascan/view/scanProgress/?scanId=";
     public static final String GENERATE_REPORT=protocol+host+":"+port+"/OTHER/core/other/htmlreport/?";
     public static final String GENERATE_REPORT2=protocol+host+":"+port+"/JSON/reports/action/generate/?title=report&template=traditional-html";
 
